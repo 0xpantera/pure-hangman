363b33295b7590919e87677991972a095ff4f242
Francososa <franky.sosa@gmail.com>
2019-04-08 23:43:40 -0600

    Define puzzle data type and functions to update puzzle state

--- src/Main.hs
+++ src/Main.hs
@@ -49,3 +49,44 @@ randomWord wl = do
 
 randomWord' :: IO String
 randomWord' = gameWords >>= randomWord
+
+
+data Puzzle = Puzzle String [Maybe Char] [Char]
+
+
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $
+     fmap renderPuzzleChar discovered)
+    ++ " Guessed so far: " ++ guessed
+
+    
+freshPuzzle :: String -> Puzzle
+freshPuzzle wordAtPlay = Puzzle wordAtPlay discovered []
+  where
+    discovered = map (const Nothing) wordAtPlay
+
+
+charInWord :: Char -> Puzzle -> Bool
+charInWord guess (Puzzle wordAtPlay _ _) = guess `elem` wordAtPlay
+
+
+alreadyGuessed :: Char -> Puzzle -> Bool
+alreadyGuessed guess (Puzzle _ _ guessed) = guess `elem` guessed
+
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar Nothing  = '_'
+renderPuzzleChar (Just c) = c
+
+
+fillInCharacter :: Char -> Puzzle -> Puzzle
+fillInCharacter c (Puzzle word filledIn s) =
+  Puzzle word newFilledIn (c : s)
+  where
+    zipper guessed wordChar guessChar =
+      if wordChar == guessed
+      then Just wordChar
+      else guessChar
+    newFilledIn =
+      zipWith (zipper c) word filledIn
